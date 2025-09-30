5642933f799d8a792c5065b1cb3b4f190f99f9a4
solpi2 <parkchini@gmail.com>
2025-09-30 11:11:08 +0900

    ValidationException

---
+++ src/main/java/com/example/exception_study/exception/ValidationException.java
@@ -0,0 +1,48 @@
+package com.example.exception_study.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ValidationException extends BusinessException {
+    private final List<ValidationError> filedErrors;
+
+    public ValidationException(String message) {
+        super("VALIDATION_ERROR", message, HttpStatus.BAD_REQUEST);
+        this.filedErrors = new ArrayList<>();
+    }
+
+    public ValidationException(List<FieldError> springFieldErrors) {
+        super("VALIDATION_ERROR", "입력값 검증에 실패했습니다", HttpStatus.BAD_REQUEST);
+
+        this.filedErrors = new ArrayList<>();
+
+        if(springFieldErrors != null) {
+            for (FieldError fieldError: springFieldErrors) {
+                this.filedErrors.add (new ValidationError(
+                        fieldError.getField(),
+                        fieldError.getRejectedValue(),
+                        fieldError.getDefaultMessage()
+                ));
+            }
+        }
+    }
+
+    public ValidationException(String field, Object rejectedValue, String message) {
+        super("VALIDATION_ERROR", "입력값 검증에 실패했습니다", HttpStatus.BAD_REQUEST);
+        this.filedErrors = List.of(new ValidationError(field, rejectedValue, message));
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public static class ValidationError {
+        private final String field;
+        private final Object rejectedValue;
+        private final String message;
+    }
+}
